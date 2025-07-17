d953c1df167d12cb5f0f873ebaa477356ba07244
ancha-mp <ancho_mp@yahoo.com>
2025-07-17 13:05:11 +0300

    Update model.py

--- model.py
+++ model.py
@@ -29,7 +29,7 @@ def preprocess_data(df: pd.DataFrame, test=True):
         
   
 def fit_and_save_model(X_df, y_df, path="data/model_weights.cbm"):
-    model = CatBoostClassifier(verbose=0)
+    model = CatBoostClassifier()
     model.fit(X_df, y_df)
 
     test_prediction = model.predict(X_df)
@@ -47,21 +47,7 @@ def load_model_and_predict(df, path="data/model_weights.cbm"):
     prediction = model.predict(df)[0]
     prediction_proba = model.predict_proba(df)[0]
     
-    encode_prediction_proba = {
-        0: "Вам не повезло с вероятностью",
-        1: "Вы выживете с вероятностью"
-    }
-
-    encode_prediction = {
-        0: "Сожалеем, вам не повезло",
-        1: "Ура! Вы будете жить"
-    }
-
-    prediction_data = {encode_prediction_proba[k]: prediction_proba[k] for k in encode_prediction_proba}
-    prediction_df = pd.DataFrame(prediction_data, index=[0])
-    prediction_label = encode_prediction.get(prediction, "Неизвестный класс")
-
-    return prediction_label, prediction_df
+    return prediction, prediction_df
 
 if __name__ == "__main__":
     df = open_data()
